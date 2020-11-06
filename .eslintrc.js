c8898b7b354ed04c1fe10d7b5bb092ed2a7b0851
Oscar Calderon <oscar.calderon@pager.com>
2020-11-06 06:24:04 -0600

    add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    "node": true,
-    es6: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-  },
+    env: {
+        commonjs: true,
+        "node": true,
+        es6: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+    },
+    rules: {
+        "indent": ["error", 4]
+    },
 };
