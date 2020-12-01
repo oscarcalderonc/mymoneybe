2932b372db33a4d7a360d02c1a5f782c0abdc743
Oscar Calderon <oscar.calderon@pager.com>
2020-12-01 14:47:57 -0600

    feat: add capability to store current balance in transaction

--- src/routes/transactions.js
+++ src/routes/transactions.js
@@ -1,38 +1,70 @@
+const { isEmpty } = require('lodash');
+
 const db = require('../db');
 const { mapDocuments } = require('../utils/utils');
 
 module.exports = (router) => {
+    router.get('/transactions', async (ctx, next) => {
+        ctx.body = mapDocuments(await db.collection('transactions').get());
+        next();
+    });
+
     router.put('/transactions', async (ctx, next) => {
         const payload = ctx.request.body;
 
         const { transactionType } = payload;
         delete payload.transactionType;
-        console.log(payload);
+
         //transform datetime
 
         try {
-            await db.collection('transactions').doc().set(payload);
-            const fromAccount = await db.collection('accounts').doc(payload.fromAccountId);
-            const { currentBalance } = (await fromAccount.get()).data();
+            const transactionRef = await db.collection('transactions').add(payload);
+            console.log(transactionRef.id);
+            const fromAccountRef = await db.collection('accounts').doc(payload.fromAccountId);
+            const { currentBalance } = (await fromAccountRef.get()).data();
 
             if (transactionType.operation !== '=') {
                 const mutableAmount = payload.amount * (transactionType.operation === '-' ? -1 : 1);
 
-                console.log(`The current balance is ${currentBalance}`);
-                console.log(`The amount is ${mutableAmount}`);
+                await fromAccountRef.set({ currentBalance: parseFloat((currentBalance + mutableAmount).toFixed(2)) }, { merge: true });
 
-                fromAccount.set({ currentBalance: parseFloat((currentBalance + mutableAmount).toFixed(2)) }, { merge: true });
+                const { currentBalance: newBalanceFromAccount } = (await fromAccountRef.get()).data();
+                transactionRef.set({ fromAccountBalance: newBalanceFromAccount }, { merge: true });
             } else {
-                const toAccount = await db.collection('accounts').doc(payload.toAccountId);
-                const { currentBalance: currentBalanceTo } = (await toAccount.get()).data();
+                const toAccountRef = await db.collection('accounts').doc(payload.toAccountId);
+                const { currentBalance: currentBalanceTo } = (await toAccountRef.get()).data();
 
-                console.log(`The current balance is ${currentBalance}`);
-                console.log(`The current balance to is ${currentBalanceTo}`);
-                console.log(`The amount is ${payload.amount}`);
+                await fromAccountRef.set({ currentBalance: parseFloat((currentBalance - payload.amount).toFixed(2)) }, { merge: true });
+                await toAccountRef.set({ currentBalance: parseFloat((currentBalanceTo + payload.amount).toFixed(2)) }, { merge: true });
 
-                fromAccount.set({ currentBalance: parseFloat((currentBalance - payload.amount).toFixed(2)) }, { merge: true });
-                toAccount.set({ currentBalance: parseFloat((currentBalanceTo + payload.amount).toFixed(2)) }, { merge: true });
+                const { currentBalance: newBalanceFromAccount } = (await fromAccountRef.get()).data();
+                const { currentBalance: newBalanceToAccount } = (await toAccountRef.get()).data();
+                transactionRef.set({ fromAccountBalance: newBalanceFromAccount, toAccountBalance: newBalanceToAccount }, { merge: true });
             }
+
+
+            // await db.collection('transactions').doc().set(payload);
+            // let fromAccountRef = await db.collection('accounts').doc(payload.fromAccountId);
+            // let toAccountRef = null;
+            // if (!isEmpty(payload.toAccountId)) {
+            //     toAccountRef = await db.collection('accounts').doc(payload.toAccountId);
+            // }
+
+            // db.runTransaction(function(transaction) {
+
+            // const { currentBalance } = (await fromAccountRef.get()).data();
+
+            // if (transactionType.operation !== '=') {
+            //     const mutableAmount = payload.amount * (transactionType.operation === '-' ? -1 : 1);
+
+            //     fromAccountRef.set({ currentBalance: parseFloat((currentBalance + mutableAmount).toFixed(2)) }, { merge: true });
+            // } else {
+                
+            //     const { currentBalance: currentBalanceTo } = (await toAccountRef.get()).data();
+
+            //     fromAccountRef.set({ currentBalance: parseFloat((currentBalance - payload.amount).toFixed(2)) }, { merge: true });
+            //     toAccountRef.set({ currentBalance: parseFloat((currentBalanceTo + payload.amount).toFixed(2)) }, { merge: true });
+            // }
 
         } catch (err) {
             console.log(err);
